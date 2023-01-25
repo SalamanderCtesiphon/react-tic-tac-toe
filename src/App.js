5dd37d38a92807cd4f3cdcbea7f9e61e9bd55884
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-25 15:29:52 -0600

    created nine squares to build the board

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+  <div>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+  </div>
+  );
 }
 
