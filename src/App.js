c1e8675664f6300a969fc8affa0cee50298cf415
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-26 10:10:42 -0600

    lifted the state of the board up to a new default function called game in order to create a history for each board state during the game

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function Square({ value, onSquareClick }) {
   </button>)   
 }
 
-export default function Board() {
+function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
